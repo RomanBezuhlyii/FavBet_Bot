89ac9b1cd34c2175ae97ae93a9db864770de85f2
DigitalOcean FavBet <bot-rletka-02@digitalocen.com>
2022-07-15 18:39:13 +0000

    fix selenoid/ fix app build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+PROJECT_PATH="/opt/deployer/FavBet_Bot"
 GIT_BRANCH="master"
+
+LAST_PATH=$(pwd)
+
+echo -e "Entring project directory: ${PROJECT_PATH}"
+cd ${PROJECT_PATH}
 
 echo -e "Configuring git client"
 git config --global user.email "bot-rletka-02@digitalocen.com"
@@ -8,3 +14,12 @@ git config --global user.name "DigitalOcean FavBet"
 
 echo -e "Pulling latest code from github.com"
 git pull origin ${GIT_BRANCH}
+
+echo -e "Build latest application version"
+docker-compose build --no-cache
+
+echo -e "Running up application"
+docker-compose up --force-recreate --build  --detach
+
+echo -e "Restoring cli context"
+cd ${LAST_PATH}
